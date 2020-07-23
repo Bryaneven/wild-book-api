3b0fe5af3588402fa8636c9829a026d22cd37b36
Bryaneven <59363064+Bryaneven@users.noreply.github.com>
2020-07-24 00:56:20 +0200

    clear console.log

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -17,8 +17,6 @@ export class AuthService {
   }
 
   async login(user: any) {
-    console.log(user);
-    
     const payload = { email : user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
